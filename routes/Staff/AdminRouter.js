bd1ad936c7952630df8729ee55e22edf5e993e71
Godsent Michael <gentlemotivator@gmail.com>
2023-05-12 14:09:52 +0100

    saved token to postman, for reusability & efficiency

--- routes/Staff/AdminRouter.js
+++ routes/Staff/AdminRouter.js
@@ -3,13 +3,15 @@ const adminRouter = express.Router();
 
 const {adminRegister,adminLogin,getSingleAdmin,getAdminProfile, getAllAdminProfile } = require('../../controllers/Staff/AdminCtrl');
 const isLoggedIn = require('../../middlewares/isLoggedIn');
+const  isAdmin  = require('../../middlewares/isAdmin');
+
 
 
 adminRouter.post('/register', adminRegister );
 adminRouter.post('/login', adminLogin );
 adminRouter.get('/', isLoggedIn,getAllAdminProfile);
-adminRouter.get('/profile', isLoggedIn,getAdminProfile );
-adminRouter.get('/:id', isLoggedIn,getSingleAdmin );
+adminRouter.get('/profile', isLoggedIn,isAdmin,getAdminProfile );
+adminRouter.get('/:id', isLoggedIn,isAdmin, getSingleAdmin );
 
 
 module.exports = adminRouter;
